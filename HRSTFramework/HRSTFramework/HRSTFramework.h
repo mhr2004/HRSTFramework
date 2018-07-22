43ad5ee87a40d12257c25a2fc1d83b4080faffa6
Hadi Roohian <hadi.roohian@zonal.co.uk>
2018-07-22 12:17:42 +0100

    Initial framework

---
+++ HRSTFramework/HRSTFramework/HRSTFramework.h
@@ -0,0 +1,19 @@
+//
+//  HRSTFramework.h
+//  HRSTFramework
+//
+//  Created by Hadi Roohian on 22/07/2018.
+//  Copyright © 2018 Sepide Taavon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HRSTFramework.
+FOUNDATION_EXPORT double HRSTFrameworkVersionNumber;
+
+//! Project version string for HRSTFramework.
+FOUNDATION_EXPORT const unsigned char HRSTFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRSTFramework/PublicHeader.h>
+
+
